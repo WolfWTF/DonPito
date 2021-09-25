fbe4fd4ee2c4d26d06327df2d2e0efa6ab203bdf
DaniCassani <danielduran1995@gmail.com>
2021-09-25 04:12:59 +0200

    a ver si elige los intervalos ahora. 3 tests para que sea más rapido el debug

--- pito.py
+++ pito.py
@@ -235,18 +235,12 @@ async def aventura(ctx):
     aciertos = 0
     media = 0
     for i in range(1,11):
-      print(i)
-      continuar, elapsed, correcto = await entrenar(ctx,modo)
+      continuar, elapsed, correcto = await entrenar(ctx,modo,interv)
       tiempo = tdf(elapsed)
       segundos_totales = round(tiempo[0]*3600*24 + tiempo[1]*3600 + tiempo[2]*60 + tiempo[3], 2)
       media += media*(i-1) + segundos_totales/i 
       if correcto:
         aciertos += 1
-
-
-
-
-
 
 ####################### PITO ##########################################################################
 
