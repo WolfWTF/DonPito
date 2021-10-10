0be4e909193357a50089bf79f32894c01b6decf1
DaniCassani <danielduran1995@gmail.com>
2021-10-11 01:05:03 +0200

    no message

--- pito.py
+++ pito.py
@@ -516,7 +516,7 @@ async def modos(ctx):
     respuesta = ":white_check_mark: Correcto! {}.".format(respuesta_correcta)
   else:
     respuesta = ":x: Incorrecto. El modo era {}, no {}.".format(respuesta_correcta, selec_usuario)
-  await ctx.reply(respuesta,reference = audio)
+  await ctx.send(respuesta,reference = audio)
 
 ################################################################################################
 ################################# PASARLE EL TOKEN AL BOT ######################################
