17c912b3005c751bee6fb24f712b99a045bfee04
DaniCassani <danielduran1995@gmail.com>
2022-01-05 12:08:11 +0100

    no message

--- pito.py
+++ pito.py
@@ -624,7 +624,7 @@ async def modos(ctx):
 async def ranking(ctx):
   padawans = aj.abrir_json("DonPito/padawans.json")
   puntuaciones = ()
-  padawans = sorted(padawans.items(), key=lambda padawans: padawans[1])    
+  padawans = sorted(padawans.items(), key="exp")    
   for ID in padawans:
     puntuaciones.append([padawans[str(ID)]["name"], padawans[str(ID)]["exp"]])
   await ctx.send(str(puntuaciones))
